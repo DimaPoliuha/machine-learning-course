79f9381e545545859b39a77a09269adc71422cb5
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-03-04 23:09:10 +0200

    K-means clustering model

--- Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
@@ -6,32 +6,35 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, 3:].values
 
-##working with missing data
-#from sklearn.preprocessing import Imputer
-#imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-#imputer = imputer.fit(X[:, 1:3])
-#X[:, 1:3] = imputer.transform(X[:, 1:3])
-#
-##encoding categorical data
-#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-#label_encoder_X = LabelEncoder()
-#x[:, 0] = label_encoder_X.fit_transform(X[:, 0])
-#one_hot_encoder = OneHotEncoder(categorical_features=[0])
-#X = one_hot_encoder.fit_transform(X).toarray()
-#label_encoder_y = LabelEncoder()
-#y = label_encoder_y.fit_transform(y)
+#using the elbow method to find the optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('elbow method')
+plt.xlabel('number of clusters')
+plt.ylabel('wcss')
+plt.show()
 
-#splitting the dataset into training set and test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+#applying k-means to the dataset
+kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10, random_state=0)
+y_kmeans = kmeans.fit_predict(X)
 
-
-##feature scaling
-#from sklearn.preprocessing import StandardScaler
-#sc_X = StandardScaler()
-#X_train = sc_X.fit_transform(X_train)
-#X_test = sc_X.transform(X_test)
+#visualising the clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s=100, c='cyan', label='cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s=100, c='magenta', label='cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='centroids')
+plt.title('cluster of clients')
+plt.xlabel('annual income (k$)')
+plt.ylabel('spending score (1-100)')
+plt.legend()
+plt.show()
