b28cdb5b9c3923015cd5f207373d1e127c176cd0
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-03-04 23:09:32 +0200

    Update kmeans.py

--- Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
@@ -27,12 +27,12 @@ kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10, random_
 y_kmeans = kmeans.fit_predict(X)
 
 #visualising the clusters
-plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='cluster 1')
-plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='cluster 2')
-plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='cluster 3')
-plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s=100, c='cyan', label='cluster 4')
-plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s=100, c='magenta', label='cluster 5')
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='centroids')
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=30, c='red', label='careful')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=30, c='blue', label='standart')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=30, c='green', label='target')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s=30, c='cyan', label='careless')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s=30, c='magenta', label='sensible')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=100, c='yellow', label='centroids')
 plt.title('cluster of clients')
 plt.xlabel('annual income (k$)')
 plt.ylabel('spending score (1-100)')
