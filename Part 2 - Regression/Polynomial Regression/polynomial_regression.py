d7c6af46f1bc99c86cd4d6742a66b97a915b58fb
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-03-11 13:31:14 +0200

    update polynomial regression

--- Part 2 - Regression/Polynomial Regression/polynomial_regression.py
+++ Part 2 - Regression/Polynomial Regression/polynomial_regression.py
@@ -10,19 +10,19 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, 2].values
 
-#fitting linear regression to the dataset
+# Fitting linear regression to the dataset
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
-#fitting polynomial regression to the dataset
+# Fitting polynomial regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree=4)
 X_poly = poly_reg.fit_transform(X)
 lin_reg2 = LinearRegression()
 lin_reg2.fit(X_poly, y)
 
-#visualising the linear regression results
+# Visualising the linear regression results
 plt.scatter(X, y, color='red')
 plt.plot(X, lin_reg.predict(X), color='blue')
 plt.title('truth or bluff (linear regression)')
@@ -30,7 +30,7 @@ plt.xlabel('position level')
 plt.ylabel('salary')
 plt.show()
 
-#visualising the polynomial regresesion results
+# Visualising the polynomial regresesion results
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 
@@ -41,8 +41,8 @@ plt.xlabel('position level')
 plt.ylabel('salary')
 plt.show()
 
-#predicting a new result with linear regression
+# Predicting a new result with linear regression
 lin_reg.predict(np.array(6.5).reshape(1, -1))
 
-#predicting a new result with polynomial regression
+# Predicting a new result with polynomial regression
 lin_reg2.predict(poly_reg.fit_transform(np.array(6.5).reshape(1, -1)))
