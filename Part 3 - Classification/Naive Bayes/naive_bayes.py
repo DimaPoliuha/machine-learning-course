34642ac88f11f72958798297a1bbbb3c1febb2e5
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-03-11 16:31:55 +0200

    update Naive Bayes

--- Part 3 - Classification/Naive Bayes/naive_bayes.py
+++ Part 3 - Classification/Naive Bayes/naive_bayes.py
@@ -10,29 +10,29 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, 2:-1].values
 y = dataset.iloc[:, 4].values
 
-#splitting the dataset into training set and test set
+# Splitting the dataset into training set and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
-##feature scaling
+# Feature scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-#fitting Naive Bayes to the training set
+# Fitting Naive Bayes to the training set
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
-#predicting the test set results
+# Predicting the test set results
 y_pred = classifier.predict(X_test)
 
-#making the confusion matrix
+# Making the confusion matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-#visualising the training set results
+# Visualising the training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2  = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
@@ -50,7 +50,7 @@ plt.ylabel('estimated salary')
 plt.legend()
 plt.show()
 
-#visualising the test set results
+# Visualising the test set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2  = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
