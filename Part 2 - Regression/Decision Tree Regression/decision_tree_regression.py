260a27f359db05c7818e0510cd591ccc81e3cc28
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-03-11 13:37:48 +0200

    update Decision Tree Regression

--- Part 2 - Regression/Decision Tree Regression/decision_tree_regression.py
+++ Part 2 - Regression/Decision Tree Regression/decision_tree_regression.py
@@ -10,21 +10,15 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, 2].values
 
-##feature scaling
-#from sklearn.preprocessing import StandardScaler
-#sc_X = StandardScaler()
-#X_train = sc_X.fit_transform(X_train)
-#X_test = sc_X.transform(X_test)
-
-#fitting decision tree to the dataset
+# Fitting decision tree to the dataset
 from sklearn.tree import DecisionTreeRegressor
 regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, y)
 
-#predicting a new result with decision tree
+# Predicting a new result with decision tree
 y_pred = regressor.predict(np.array(6.5).reshape(1, -1))
 
-#visualising the regresesion model results
+# Visualising the regression model results
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='red')
