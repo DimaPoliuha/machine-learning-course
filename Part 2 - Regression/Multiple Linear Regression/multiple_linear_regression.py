4f3eddab80f72523c920de00f5b7789fb5a78473
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-03-11 13:22:29 +0200

    update multiple linear regression

--- Part 2 - Regression/Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Multiple Linear Regression/multiple_linear_regression.py
@@ -1,4 +1,4 @@
-#multiple linear regression
+# Multiple Linear Regression
 
 # Importing the libraries
 import numpy as np
@@ -10,33 +10,32 @@ dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
-##encoding categorical data
-#encoding the independant variable
+# eEcoding the independant variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 label_encoder_X = LabelEncoder()
 X[:, 3] = label_encoder_X.fit_transform(X[:, 3])
 one_hot_encoder = OneHotEncoder(categorical_features=[3])
 X = one_hot_encoder.fit_transform(X).toarray()
 
-#avoiding the dummy variable trap
+# Avoiding the dummy variable trap
 X = X[:, 1:]
 
-#splitting the dataset into training set and test set
+# Splitting the dataset into training set and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-#fitting multiple linear regression to the training set
+# Fitting multiple linear regression to the training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
-#predicting the test set results
+# Predicting the test set results
 y_pred = regressor.predict(X_test)
 
 
-#building the optimal model using backward elimination
+# Building the optimal model using backward elimination
 import statsmodels.formula.api as sm
-#append column of 1-s to the start of array X (intercept)
+# Append column of 1-s to the start of array X (intercept)
 X = np.append(arr=np.ones((50,1)).astype(int), values=X, axis=1)
 
 X_opt = X[:, [0,1,2,3,4,5]]
