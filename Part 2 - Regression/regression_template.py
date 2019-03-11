e5b88cc8b75f3a98ae6fa73b0ee29b2b6e3f0c0d
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-03-11 13:49:07 +0200

    Update regression_template.py

--- Part 2 - Regression/regression_template.py
+++ Part 2 - Regression/regression_template.py
@@ -1,4 +1,4 @@
-#regression template
+# Regression Template
 
 # Importing the libraries
 import numpy as np
@@ -10,23 +10,23 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, 2].values
 
-#splitting the dataset into training set and test set
+# Splitting the dataset into training set and test set
 #from sklearn.model_selection import train_test_split
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-##feature scaling
+# Feature scaling
 #from sklearn.preprocessing import StandardScaler
 #sc_X = StandardScaler()
 #X_train = sc_X.fit_transform(X_train)
 #X_test = sc_X.transform(X_test)
 
-#fitting regression model to the dataset
+# Fitting regression model to the dataset
 #create regressor here
 
-#predicting a new result with regression 
+# Predicting a new result with regression 
 y_pred = regressor.predict(np.array(6.5).reshape(1, -1))
 
-#visualising the regresesion model results
+# Visualising the regression model results
 plt.scatter(X, y, color='red')
 plt.plot(X, regressor.predict(X), color='blue')
 plt.title('truth or bluff (regression model)')
@@ -34,7 +34,7 @@ plt.xlabel('position level')
 plt.ylabel('salary')
 plt.show()
 
-#visualising the regresesion model results (for higher resolution and smoother curve)
+# Visualising the regression model results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='red')
